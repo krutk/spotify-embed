0cd84ab6ea0c7cfdb7580d75039e48f789a06a54
Kumar Utkarsh <utkarshroy28@gmail.com>
2023-10-14 04:18:16 +0530

    new implementation

---
+++ src/components/Spotify/Spotify.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+type Props = {
+  key: string;
+  url: string;
+  bordeRRadius?: number;
+  width?: number | string;
+  height?: number | string;
+  alternateTheme?: string;
+};
+
+const Spotify: React.FC<Props> = ({
+  url,
+  bordeRRadius,
+  width,
+  height,
+  alternateTheme,
+}) => {
+  const match = url.match(/(album|track|episode|playlist)\/(\w+)/);
+  const id = match?.[1] + "/" + match?.[2];
+  const themeQueryParam = alternateTheme ? "?theme=0" : "";
+  return (
+    <iframe
+      style={{ borderRadius: bordeRRadius ? bordeRRadius.toString() : "12px" }}
+      src={`https://open.spotify.com/embed/${id}${themeQueryParam}`}
+      width={
+        width === "wide"
+          ? "100%"
+          : width === "compact"
+          ? "40%"
+          : !width
+          ? "100%"
+          : width
+      }
+      height={height === "compact" ? "152" : !height ? "352" : height}
+      allowFullScreen
+      allow="encrypted-media"
+      loading="lazy"
+    ></iframe>
+  );
+};
+
+export default Spotify;
